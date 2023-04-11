d8416eaab97f11afd0a1ccd54dadc622818cffe8
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2023-04-11 20:20:27 -0300

    feat: add lotteryApi and axios

---
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -0,0 +1,26 @@
+import {
+  type HttpRequest,
+  type HttpResponse,
+  type HttpClient
+} from '../../../application/helpers/https'
+import axios, { type AxiosResponse } from 'axios'
+
+export class AxiosHttpClient implements HttpClient {
+  async request (data: HttpRequest): Promise<HttpResponse> {
+    let axiosResponse: AxiosResponse
+    try {
+      axiosResponse = await axios.request({
+        url: data.url,
+        method: data.method,
+        data: data.body,
+        headers: data.headers
+      })
+    } catch (error: any) {
+      axiosResponse = error.response
+    }
+    return {
+      statusCode: axiosResponse.status,
+      body: axiosResponse.data
+    }
+  }
+}
