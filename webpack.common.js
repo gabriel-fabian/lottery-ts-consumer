2ee9e32b21d2fc74f7d2effd771ed0bdc7e2937c
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2023-04-11 11:02:36 -0300

    feat: add webpack config

---
+++ webpack.common.js
@@ -0,0 +1,15 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+  entry: './src/main/index.tsx',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'main-bundle-[hash].js',
+    publicPath: '/'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', 'scss']
+  },
+  plugins: [new CleanWebpackPlugin()]
+}
---
+++ webpack.dev.js
@@ -0,0 +1,55 @@
+const { DefinePlugin } = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const common = require('./webpack.common')
+const { merge } = require('webpack-merge')
+
+module.exports = merge(common, {
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true
+            }
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: {
+      directory: './public'
+    },
+    historyApiFallback: true,
+    devMiddleware: {
+      writeToDisk: true
+    }
+  },
+  plugins: [
+    new DefinePlugin({
+      'process.env.API_URL': JSON.stringify(
+        'https://servicebus2.caixa.gov.br/portaldeloterias/api/home/ultimos-resultados'
+      )
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/template.dev.html',
+      favicon: './public/favicon.ico'
+    })
+  ]
+})
