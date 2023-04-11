42770b7a062ed1234cb08e18e5d03cf3f5892aeb
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2023-04-11 11:02:50 -0300

    feat: add lottery page

---
+++ src/main/factories/pages/lottery-factory.tsx
@@ -0,0 +1,8 @@
+import Lottery from '../../../presentation/pages/lottery'
+import React from 'react'
+
+const MakeLottery: React.FC = () => {
+  return <Lottery />
+}
+
+export default MakeLottery
---
+++ src/presentation/pages/lottery.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Lottery: React.FC = () => {
+  return <div>Lottery Page</div>
+}
+
+export default Lottery
