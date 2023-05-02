f73c5ddc861b6a6701db8a87eb934e3b7fce2166
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2023-05-01 22:22:01 -0300

    refactor: update app composition to createRoot

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,6 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
 import '../presentation/styles/global.scss'
+
+import React from 'react'
+import ReactDOMClient from 'react-dom/client'
 import Router from './routes/router'
 
-ReactDOM.render(<Router />, document.getElementById('main'))
+const rootElement = document.getElementById('main') as Element
+
+const root = ReactDOMClient.createRoot(rootElement)
+
+root.render(<Router />)
